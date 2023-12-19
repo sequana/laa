278320b8cb0cc80f4fc13517fe5fdb9ccddd0c08
Thomas Cokelaer <cokelaer@gmail.com>
2023-12-19 12:15:28 +0100

    Update to use click/pyproject/final apptainers

--- sequana_pipelines/laa/main.py
+++ sequana_pipelines/laa/main.py
@@ -12,92 +12,57 @@
 ##############################################################################
 import sys
 import os
-import argparse
 
-from sequana_pipetools.options import *
-from sequana_pipetools.misc import Colors
-from sequana_pipetools.info import sequana_epilog, sequana_prolog
+import click_completion
+import rich_click as click
 from sequana_pipetools import SequanaManager
+from sequana_pipetools.options import *
 
-col = Colors()
+click_completion.init()
 
 NAME = "laa"
 
 
-class Options(argparse.ArgumentParser):
-    def __init__(self, prog=NAME, epilog=None):
-        usage = col.purple(sequana_prolog.format(**{"name": NAME}))
-        super(Options, self).__init__(
-            usage=usage,
-            prog=prog,
-            description="",
-            epilog=epilog,
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        )
-
-        # add a new group of options to the parser
-        so = SlurmOptions()
-        so.add_options(self)
-
-        # add a snakemake group of options to the parser
-        so = SnakemakeOptions(working_directory=NAME)
-        so.add_options(self)
-
-        so = InputOptions()
-        so.add_options(self)
-
-        so = GeneralOptions()
-        so.add_options(self)
-
-        pipeline_group = self.add_argument_group("pipeline")
-
-        pipeline_group.add_argument("--reference-file", dest="reference", required=True, type=str)
-
-    def parse_args(self, *args):
-        args_list = list(*args)
-        if "--from-project" in args_list:
-            if len(args_list) > 2:
-                msg = (
-                    "WARNING [sequana]: With --from-project option, "
-                    + "pipeline and data-related options will be ignored."
-                )
-                print(col.error(msg))
-            for action in self._actions:
-                if action.required is True:
-                    action.required = False
-        options = super(Options, self).parse_args(*args)
-        return options
+help = init_click(
+    NAME,
+    groups={
+        "Pipeline Specific": [
+            "--reference-file",
+        ],
+    },
+)
 
 
-def main(args=None):
+@click.command(context_settings=help)
+@include_options_from(ClickInputOptions, add_input_readtag=False)
+@include_options_from(ClickSnakemakeOptions, working_directory=NAME)
+@include_options_from(ClickSlurmOptions)
+@include_options_from(ClickGeneralOptions)
+@click.option("--reference-file", "reference", required=True, type=click.Path(dir_okay=False, file_okay=True))
+@click.option("--annotation-file", "annotation", default="", show_default=True, required=False, type=click.Path(dir_okay=False, file_okay=True))
+def main(**options):
 
-    if args is None:
-        args = sys.argv
-
-    # whatever needs to be called by all pipeline before the options parsing
-    from sequana_pipetools.options import before_pipeline
-
-    before_pipeline(NAME)
-
-    # option parsing including common epilog
-    options = Options(NAME, epilog=sequana_epilog).parse_args(args[1:])
-
+    if options["from_project"]:
+        click.echo("--from-project Not yet implemented")
+        sys.exit(1)
 
     # the real stuff is here
     manager = SequanaManager(options, NAME)
-
-    # create the beginning of the command and the working directory
     manager.setup()
 
-    if options.from_project is None:
-        # fill the config file with input parameters
-        cfg = manager.config.config
-        # EXAMPLE TOREPLACE WITH YOUR NEEDS
-        cfg.input_directory = os.path.abspath(options.input_directory)
-        cfg.input_pattern = options.input_pattern
-        manager.exists(cfg.input_directory)
+    # aliases
+    #options = manager.options
+    cfg = manager.config.config
 
-        cfg.reference_file = os.path.abspath(options.reference)
+    from sequana_pipetools import logger
+    logger.setLevel(options['level'])
+
+    cfg.input_pattern = options['input_pattern']
+    cfg.input_directory = os.path.abspath(options['input_directory'])
+
+    cfg.reference_file = os.path.abspath(options['reference'])
+    
+    cfg.annotation_file = os.path.abspath(options['annotation'])
 
     # finalise the command and save it; copy the snakemake. update the config
     # file and save it.
